d196323637d02668f52df37f3639a10296fa0bb7
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-07-08 13:03:24 +0300

    navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,11 @@
 import "./Styles/main.scss";
 import React from "react";
-import logo from "./assets/images/logo.png";
-import logo2 from "./assets/images/logo2.png";
+import Nav from "./Components/Nav";
+
 
 function App() {
   return (
-    <div>
-      <img src={logo} alt="" />
-      <img src={logo2} alt="" />
-    </div>
+    <Nav/>
   );
 }
 

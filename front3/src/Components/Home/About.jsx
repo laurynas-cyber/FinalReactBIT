e0d82ee981a8187ca1cbae37ef3266c4b095da67
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-09-18 12:48:36 +0300

    carcase ready

--- front3/src/Components/Home/About.jsx
+++ front3/src/Components/Home/About.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 export default function About() {
-  return <div className="container p-0 contentDown">
-    
+  return (
+    <div className="container p-0 contentDown AboutContainer">
+      {/* <div className="AboutItemsContainer">
 
 
-  </div>;
+    </div> */}
+      <div className="EmailContacts">Email</div>
+      <div className="SocialContainer">
+        <div className="GitHubContacts">
+          <span> GitHub </span> <span>laurynas-cyber </span>
+        </div>
+        <div className="LinkedInContacts">
+          <span>LinkedIn </span> <span>Laurynas Stančiauskas </span>
+        </div>
+      </div>
+    </div>
+  );
 }

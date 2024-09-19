b76fb2c8d0898b18adf67572bf11fb53a241b4af
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-09-19 12:38:17 +0300

    scrollbar added

--- front3/src/Components/Home/Donors/DonorsPostCard.jsx
+++ front3/src/Components/Home/Donors/DonorsPostCard.jsx
@@ -14,6 +14,32 @@ function DonorsPostCard() {
         </div>
         <div className="DonorsPostCard-UsersListContainer">
           <div className="UsersListContainer-SearchSection">Search</div>
+          <div className="DonorsListContainer">
+            <div className="DonorsListBorders-left"></div>
+            <div className="DonorsListScrollBar">
+              <div className="DonorsListScrollBar-infoSection">
+                <div className="DonorsListScrollBar-infoSection-name">Name</div>
+                <div className="DonorsListScrollBar-infoSection-email">
+                  Email
+                </div>
+                <div className="DonorsListScrollBar-infoSection-donation">
+                  Donation Eur
+                </div>
+              </div>
+              <div className="DonorsList-user">
+                <div className="DonorsListScrollBar-infoSection-name DonorsList-user-divide ">
+                  NAME
+                </div>
+                <div className="DonorsListScrollBar-infoSection-email DonorsList-user-divide">
+                  EMAIL
+                </div>
+                <div className="DonorsListScrollBar-infoSection-donation DonorsList-user-divide">
+                  DONATION
+                </div>
+              </div>
+            </div>
+            <div className="DonorsListBorders-right"></div>
+          </div>
         </div>
       </div>
     </div>
